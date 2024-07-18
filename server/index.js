290e5ff36df5c830a9241cca21f2f436b3fef904
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-17 19:21:31 -0700

    Initialize project structure

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 
-const PORT = 4000;
+const PORT = 4003;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
