88f970ffa7297e18838ac21545d50f520f69936e
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-23 21:43:30 -0700

    Move getting current year outside of JSX, and add aria labeling for links in footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 export default function Footer () {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="py-4 bg-gray-900 dark:bg-gray-800">
             <div className="container mx-auto px-4">
-                <p className="text-white text-center">&copy; {new Date().getFullYear()} Sirus Salari. All rights reserved.</p>
+                <p className="text-white text-center">&copy; {currentYear} Sirus Salari. All rights reserved.</p>
                 <div className="flex justify-center mt-4">
-                    <a href='https://www.linkedin.com/in/salari-insite/' target='_blank' rel='noopener noreferrer' className="text-white mx-2">LinkedIn</a>
-                    <a href='https://github.com/sirus-the-beaver' target='_blank' rel='noopener noreferrer' className="text-white mx-2">GitHub</a>
-                    <a href='https://scrappedscript.com/' target='_blank' rel='noopener noreferrer' className="text-white mx-2">Blog</a>
+                    <a href='https://www.linkedin.com/in/salari-insite/' target='_blank' rel='noopener noreferrer' className="text-white mx-2" aria-label="LinkedIn">LinkedIn</a>
+                    <a href='https://github.com/sirus-the-beaver' target='_blank' rel='noopener noreferrer' className="text-white mx-2" aria-label="GitHub">GitHub</a>
+                    <a href='https://scrappedscript.com/' target='_blank' rel='noopener noreferrer' className="text-white mx-2" aria-label="Blog">Blog</a>
                 </div>
             </div>
         </footer>
