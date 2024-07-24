c0de098ac85b93d137ec0986d44112a1f9d8aff6
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-23 20:11:30 -0700

    Add info about chatbot

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,21 @@ import Chatbot from '../components/Chatbot';
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-screen dark:bg-gray-900">
+        <div className="flex flex-col text-white font-serif justify-center items-center dark:bg-gray-900">
+            <div className="max-w-lg mx-auto p-4">
+                <p className="text-center">Welcome to my portfolio site! Feel free to ask questions to the chatbot below.</p>
+                <p className="text-center">This assistant leverages OpenAI's API and a fine-tuned GPT-3.5-turbo model to provide detailed insights about my projects, skills, and interests.</p>
+                <p className="text-center">Utilizing the "natural" library for Node.js, user queries are preprocessed and matched to the most relevant data using advanced NLP techniques.</p>
+                <p className="text-center">Some examples of questions you might want to ask include:</p>
+                <ul className="list-disc list-inside">
+                    <li>"How has understanding x86 Assembly improved your grasp of low-level programming concepts?"</li>
+                    <li>"Share your experience with building your first software application."</li>
+                    <li>"Share your thoughts on the future of software engineering."</li>
+                    <li>"Share about the type of music you're interested in."</li>
+                </ul>
+                <p className="text-center">This chatbot is designed to deliver precise and context-aware responses, making your interaction both informative and engaging</p>
+                <p className="text-center">Dive in and explore the breadth of my technical and creative pursuits!</p>
+            </div>
             <Chatbot />
         </div>
     )
--- client/src/pages/Skills.jsx
+++ client/src/pages/Skills.jsx
@@ -24,8 +24,8 @@ export default function Skills () {
     return (
         <div className="flex justify-center bg-gray-900">
             <div className="max-w-4xl w-full mt-4 mb-4">
-                <h1 className="text-3xl font-bold text-white mb-4 font-serif">Skills</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <h1 className="text-4xl font-bold mb-8 text-center font-serif text-white">Skills</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
                     {skills.map((skill) => {
                         const skillProjects = skill.projectIDs.map(projectId => projects.find(project => project.id === projectId));
                         return <SkillCard key={skill.name} skill={skill} projects={skillProjects} />
