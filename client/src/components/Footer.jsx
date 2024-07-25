0d4d93e89e96cd194f82d8c40fc8cc21679f9e9c
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-24 18:46:47 -0700

    Update styling for footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaBlog } from 'react-icons/fa';
 
 export default function Footer () {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="py-4 bg-gray-900 dark:bg-gray-800">
-            <div className="container mx-auto px-4">
-                <p className="text-white text-center">&copy; {currentYear} Sirus Salari. All rights reserved.</p>
-                <div className="flex justify-center mt-4">
-                    <a href='https://www.linkedin.com/in/salari-insite/' target='_blank' rel='noopener noreferrer' className="text-white mx-2" aria-label="LinkedIn">LinkedIn</a>
-                    <a href='https://github.com/sirus-the-beaver' target='_blank' rel='noopener noreferrer' className="text-white mx-2" aria-label="GitHub">GitHub</a>
-                    <a href='https://scrappedscript.com/' target='_blank' rel='noopener noreferrer' className="text-white mx-2" aria-label="Blog">Blog</a>
+        <footer className="py-6 bg-gradient-to-r from-gray-800 to-gray-900 text-white">
+            <div className="container mx-auto px-4 text-center">
+                <p className="mb-4">&copy; {currentYear} Sirus Salari. All rights reserved.</p>
+                <div className="flex justify-center space-x-4">
+                    <a href='https://www.linkedin.com/in/salari-insite/' target='_blank' rel='noopener noreferrer' className="hover:text-blue-500 transition-colors duration-300" aria-label="LinkedIn">
+                        <FaLinkedin size={24} />
+                    </a>
+                    <a href='https://github.com/sirus-the-beaver' target='_blank' rel='noopener noreferrer' className="hover:text-gray-400 transition-colors duration-300" aria-label="GitHub">
+                        <FaGithub size={24} />
+                    </a>
+                    <a href='https://scrappedscript.com/' target='_blank' rel='noopener noreferrer' className="hover:text-yellow-500 transition-colors duration-300" aria-label="Blog">
+                        <FaBlog size={24} />
+                    </a>
                 </div>
             </div>
         </footer>
