2ce463ce61cdf6905cc65cca6068fa436dd22cb5
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-25 01:43:51 -0700

    remove helmet since it's effecting fetching from Hashnode API

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser';
 import processQuery from './services/chatbot.js';
 import dotenv from 'dotenv';
 import rateLimit from 'express-rate-limit';
-import helmet from 'helmet';
 import { body, validationResult } from 'express-validator';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -14,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 dotenv.config();
-app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
