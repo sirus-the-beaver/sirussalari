8d903fa7e041a84eb6390fced421f25e699d8c51
Sirus Salari <salaris@oregonstate.edu>
2025-08-13 02:15:53 -0700

    Update About page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/Home';
 import Skills from './pages/Skills';
 import Projects from './pages/Projects';
 import Blog from './pages/Blog';
